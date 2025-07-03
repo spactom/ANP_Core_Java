34a8c1b0beac349f3c998a6e2c5904f0daee0bd7
spactom <kishanmakwana1504@gmail.com>
2025-07-03 12:22:02 +0530

    Add files via upload

---
+++ Overloading.java
@@ -0,0 +1,44 @@
+package ANP;
+
+class Meth_Overloading {
+
+	void Area(int radius) {
+
+		double area = 3.14 * radius * radius;
+
+		System.out.print("Area Of Circle is :" + area+"\n");
+	}
+
+	void Area(int length, int width) {
+		double area = length * width;
+
+		System.out.print("Area of Reactangle is :" + area+"\n");
+	}
+
+	void Area(int base, int height) {
+		double area = base * height;
+
+		System.out.print("Area of Triangle is :" + area);
+	}
+
+}
+
+public class Overloading {
+
+	public static void main(String[] args) {
+
+		Meth_Overloading obj = new Meth_Overloading();
+		obj.Area(5);
+		obj.Area(5,10);
+		obj.Area(10,20);
+
+	}
+
+}
+
+
+//Output :
+//	
+//	Area Of Circle is :78.5
+//	Area of Reactangle is :50.0
+//	Area of Reactangle is :200.0
